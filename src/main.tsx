86f0e9c94574f39e2fdad2a193dc494751fe4b66
saky0504 <saky0504@gmail.com>
2025-10-22 03:23:03 +0900

    fix: use React import for Speed Insights in Vite project

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '../styles/globals.css'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
