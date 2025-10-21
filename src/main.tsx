dd573847fe5d7f003c117286624c4247c60cd24f
saky0504 <saky0504@gmail.com>
2025-10-22 03:43:21 +0900

    feat: add Speed Insights with correct React component

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '../styles/globals.css'
-import { injectSpeedInsights } from '@vercel/speed-insights'
-
-// Speed Insights 초기화
-injectSpeedInsights()
+import { SpeedInsights } from '@vercel/speed-insights'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
